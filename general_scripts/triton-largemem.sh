d1c07f0a6701dd5e34bc9369f241b29906714ecf
Karolis Uziela <karolis.uziela@scilifelab.se>
2014-03-27 19:06:03 +0200

    deleted general_scripts dir

--- general_scripts/triton-largemem.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-#SBATCH --time=7-00:00:00 --mem-per-cpu=8000
-#SBATCH -p largemem
-#SBATCH -c 4
-
-srun <CHANGE>
--- general_scripts/triton-short.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-#SBATCH --time=0-04:00:00 --mem-per-cpu=4000
-#SBATCH -p short
-
-srun <CHANGE>
